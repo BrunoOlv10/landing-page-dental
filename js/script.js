2c01eb21971428aacf7f8541dbc7cf56037d5018
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-07-27 23:05:12 -0300

    add details

---
+++ js/script.js
@@ -0,0 +1,64 @@
+function mudarFace() {
+    var face = document.getElementById('face').getElementsByTagName('img')[0];
+    face.src = '../img/face-logo-azul.png';
+  }
+  
+  function voltarFace() {
+    var face = document.getElementById('face').getElementsByTagName('img')[0];
+    face.src = '../img/face-logo.png';
+  }
+
+
+function mudarInsta() {
+    var insta = document.getElementById('insta').getElementsByTagName('img')[0];
+    insta.src = '../img/insta-logo-azul.png';
+  }
+  
+    function voltarInsta() {
+    var insta = document.getElementById('insta').getElementsByTagName('img')[0];
+    insta.src = '../img/insta-logo.png';
+  }
+
+
+function mudarLinkedin() {
+    var linkedin = document.getElementById('linkedin').getElementsByTagName('img')[0];
+    linkedin.src = '../img/linkedin-logo-azul.png';
+  }
+  
+    function voltarLinkedin() {
+    var linkedin = document.getElementById('linkedin').getElementsByTagName('img')[0];
+    linkedin.src = '../img/linkedin-logo.png';
+  }
+
+
+function mudarTwitter() {
+    var twitter = document.getElementById('twitter').getElementsByTagName('img')[0];
+    twitter.src = '../img/twitter-logo-azul.png';
+  }
+  
+    function voltarTwitter() {
+    var twitter = document.getElementById('twitter').getElementsByTagName('img')[0];
+    twitter.src = '../img/twitter-logo.png';
+  }
+  
+
+function mudarLogo() {
+      var logo = document.getElementById('logo-dental').getElementsByTagName('img')[0];
+      logo.src = '../img/logo.png';
+  }
+    
+    function voltarLogo() {
+      var logo = document.getElementById('logo-dental').getElementsByTagName('img')[0];
+      logo.src = '../img/logo-branca.png';
+  }
+
+  
+  function mudarTexto() {
+        var logo = document.getElementById('logo-dental').getElementsByTagName('img')[0];
+        logo.src = '../img/logo.png';
+    }
+      
+      function voltarTexto() {
+        var logo = document.getElementById('logo-dental').getElementsByTagName('img')[0];
+        logo.src = '../img/logo-branca.png';
+    }
