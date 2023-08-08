39e57d0d4b15df607cdf1ec7e0ab267d86c9bcf6
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-08-08 01:43:35 -0300

    testes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,53 @@
-function clickMenu () {
-  if (itens.style.display == 'block') {
-      itens.style.display = 'none'
-  } else {
-      itens.style.display = 'block'
+// function clickMenu () {
+//   if (itens.style.display == 'block') {
+//       itens.style.display = 'none'
+//   } else {
+//       itens.style.display = 'block'
+//   }
+// }
+
+// function mudouTamanho() {
+//   if (window.innerWidth >= 768) {
+//   itens.style.display = 'none'
+//   } else {
+//   itens.style.display = 'block'
+//   }
+// }
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelector(navLinks);
+    this.activeClass = "active";
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this)
+    this.navList.classList.toggle(this.activeClass);
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener('click', this.handleClick); 
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
   }
 }
 
-function mudouTamanho() {
-  if (window.innerWidth >= 768) {
-  itens.style.display = 'none'
-  } else {
-  itens.style.display = 'block'
-  }
-}
+MobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+MobileNavbar.init();
+
 
 function mudarFace() {
     var face = document.getElementById('face').getElementsByTagName('img')[0];
